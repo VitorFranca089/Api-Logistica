ca9a1dc9897d0b53846396bd65f18341c4e37d6c
Vitor Franca <vitorfranca089@gmail.com>
2024-08-02 15:19:25 -0300

    feat: add EnderecoUtils component to get or create a address in DB

---
+++ src/main/java/com/logistica/api/service/impl/EnderecoUtils.java
@@ -0,0 +1,25 @@
+package com.logistica.api.service.impl;
+
+import com.logistica.api.dto.EnderecoDTO;
+import com.logistica.api.model.Endereco;
+import com.logistica.api.repository.EnderecoRepository;
+import com.logistica.api.service.ViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnderecoUtils {
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private ViaCepService viaCepService;
+
+    public Endereco obterOuCriarEndereco(EnderecoDTO enderecoDTO) {
+        Endereco endereco = this.viaCepService.getEndereco(enderecoDTO.cep());
+        if(!enderecoDTO.unidade().isEmpty()) endereco.setUnidade(enderecoDTO.unidade());
+        return enderecoRepository.save(endereco);
+    }
+
+}
