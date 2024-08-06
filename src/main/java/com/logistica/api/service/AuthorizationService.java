738504b9c1309499d48b730ea91ed824d5bdff6d
Vitor Franca <vitorfranca089@gmail.com>
2024-08-06 17:19:57 -0300

    feat: create AuthorizationService

---
+++ src/main/java/com/logistica/api/service/AuthorizationService.java
@@ -0,0 +1,21 @@
+package com.logistica.api.service;
+
+import com.logistica.api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return this.usuarioRepository.findByEmail(email);
+    }
+
+}
